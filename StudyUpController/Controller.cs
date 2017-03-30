7cc6bc6d99dedbd9e229b21b362453e535d251b7
maayan hassidim <hassidim@post.bgu.ac.il>
2017-03-31 01:40:39 +0300

    hdh

--- StudyUpController/Controller.cs
+++ StudyUpController/Controller.cs
@@ -107,6 +107,9 @@ namespace StudyUpController
             return m_model.UploadMaterial(newMaterial);
         }
 
-       
+        public List<Material> RetreiveMaterialsAdvancedSearch(string tag)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
